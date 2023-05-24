bdbed4a08a17c6309e3e9d80f2da8be624cfae59
Aman Kumar <61074175+amanjaiswa@users.noreply.github.com>
2023-05-24 19:46:43 +0530

    Update authentication.js

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
   })
 );
